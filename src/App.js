e8e71e3e3cfa8ac40d44eb7e1c5bdfbdd34d0710
ana-biscalchin <anacarolinabis@gmail.com>
2019-07-23 12:56:50 -0300

    list de candidatos em material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet"> </link>
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PaperSheet from "./components/list";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <section>
+        <PaperSheet />
+      </section>
     </div>
   );
 }
