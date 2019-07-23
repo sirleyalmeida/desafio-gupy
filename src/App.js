d466a5acf5994a36d2d1b642fd57c914d5103da7
ana-biscalchin <anacarolinabis@gmail.com>
2019-07-23 11:23:52 -0300

    instalação do material ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet"> </link>
+
 function App() {
   return (
     <div className="App">
